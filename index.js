d2c61be85f0c17f33b8e66827d5e8b3106dab068
Narkz <aninatsumi@gmail.com>
2023-02-12 23:36:45 +0700

    luu tien do

---
+++ index.js
@@ -0,0 +1,5 @@
+var change_Name = document.getElementsByid('change_Name');
+
+change_Name.addEventListener('click', function() {
+    document.getElementById('pop_up_change_name').style.display = "block";
+})
